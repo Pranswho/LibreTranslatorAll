b3159e8bc1bcf389e2b2f0fb80db180ae03945df
Pranswho <126380060+Pranswho@users.noreply.github.com>
2023-05-20 13:10:12 +0530

    Update Translation.py

--- Translation.py
+++ Translation.py
@@ -1,19 +1,35 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 from tkinter import filedialog
+from tkinter import *
 from tkinter.filedialog import askopenfile
 from tkPDFViewer import tkPDFViewer as pdf
 import PyPDF2
 from fpdf import FPDF
 from libretranslatepy import LibreTranslateAPI
+  
+    
 my_w = tk.Tk()
 my_w.geometry("400x300")  # Size of the window 
 my_w.title('Libre Translator')
 my_font1=('times', 18, 'bold')
 l1 = tk.Label(my_w,text='Add PDF file to be translated',width=30,font=my_font1)  
 l1.grid(row=1,column=1)
+n = tk.StringVar()
+languages={"de":"German","fr":"French","es":"Spanish","pt":"Portugese"}
+languagechoose = ttk.Combobox(my_w, width = 27, textvariable = n)
+languagechoose['values'] = ('German', 'French','Spanish','Portugese')
+languagechoose.grid(row=2,column=1)
+languagechoose.current(0)
+
+
 b1 = tk.Button(my_w, text='Upload File', 
    width=20,command = lambda:upload_file())
-b1.grid(row=2,column=1) 
+b1.grid(row=3,column=1)
+
+           
+
+
 
 def translate(text, source_lang, language):
     translator = LibreTranslateAPI()
@@ -43,11 +59,14 @@ def upload_file():
 
         for pageObj in pdf_file.pages:
             extracted_text += pageObj.extract_text()
-        translated_text = translate(extracted_text, "en", "de")    
+        for key, value in languages.items():
+            if(value==languagechoose.get()):
+                tr=key
+        translated_text = translate(extracted_text, "en", tr)    
         write_pdf(output_file, translated_text)
 
 
-output_file = 'C:/Users/prans/OneDrive/Desktop/Libre/output.pdf'
+output_file = 'output.pdf'
         
 
 my_w.mainloop()  # Keep the window open
